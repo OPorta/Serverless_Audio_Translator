99fec2d4210446e6dec2a2c36652db539a414d31
EC2 Default User <ec2-user@ip-172-31-31-113.eu-west-1.compute.internal>
2020-10-12 19:15:33 +0000

    improving input-samples script

--- input-samples/execute_sample.sh
+++ input-samples/execute_sample.sh
@@ -1,2 +1,4 @@
 UPLOAD_BUCKET_NAME=$(aws cloudformation describe-stack-resource --stack-name audio-translator-transcribe-audio-and-send-transcription-to-sns --logical-resource-id UploadBucket --query "StackResourceDetail.PhysicalResourceId" --output text)
-aws s3api put-object --bucket ${UPLOAD_BUCKET_NAME} --key sample-2.wav --body ./8323__levinj__powerwords-english.wav
+NEW_FILE=sample-${RANDOM}.wav
+echo "New File: $NEW_FILE"
+aws s3api put-object --bucket ${UPLOAD_BUCKET_NAME} --key $NEW_FILE --body ./8323__levinj__powerwords-english.wav
